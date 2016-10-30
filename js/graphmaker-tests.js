21713af5b2cad828d27b81bc34269b9c46a14322
Nick Field <heyimnick@live.com>
2016-10-30 17:30:40 +0000

    organized tests

--- js/graphmaker-tests.js
+++ js/graphmaker-tests.js
@@ -17,14 +17,14 @@ QUnit.module( "Setup & JSON Retrieval", {
 
 
 /* QUnit */
-QUnit.test("QUNit init test", function(assert){
+QUnit.test("QUNit Init", function(assert){
 
     assert.ok(1 === 1, 'Testing on 1 === 1');
 });
 
 
 /* Graphmaker Setup*/
-QUnit.test("getFormattedDate should return correctly formatted date", function(assert){
+QUnit.test("getFormattedDate", function(assert){
 
     assert.equal(
         this.graphmaker.getFormattedDate("July 21, 1983", "yyyy-mm-dd"),
@@ -32,14 +32,10 @@ QUnit.test("getFormattedDate should return correctly formatted date", function(a
                                             "Should return correctly formatted date");
 });
 
-QUnit.test("Graphmaker has prepared url", function(assert){
+/* getRawDataAsync */
+QUnit.test("getRawDataAsync", function(assert){
 
-    assert.ok(this.graphmaker.url,"has url: " + this.graphmaker.url);
-});
-
-
-/* GOOD JSON request */
-QUnit.test("retrieveJSON can take GOOD url and retrieve JSON data asynchronously", function(assert){
+    assert.ok(true, "retrieveJSON can take GOOD url and retrieve JSON data asynchronously")
 
     var done = assert.async();
     /* Uses callback to check for errors */
@@ -51,26 +47,6 @@ QUnit.test("retrieveJSON can take GOOD url and retrieve JSON data asynchronously
             done();
         });
 });
-
-// Uncomment to check for JSON errors
-// /* Bad JSON request */
-// QUnit.test("retrieveJSON can take BAD url and return correct error", function(assert){
-//
-//     var done = assert.async();
-//
-//     /* Uses callback to check for errors */
-//     this.graphmaker.rawData =
-//         this.graphmaker.getRawDataAsync("lol.html", {method: "GET"},
-//         function(error, success){
-//             if (error) assert.ok(true, "Handles error: " + error);
-//             else assert.ok(false);
-//             done();
-//         });
-//
-// });
-
-
-
 
 
 //// 2. Handling Data ////
@@ -101,7 +77,7 @@ QUnit.module( "Data Handling", {
     }});
 
 // Assures raw data exists
-QUnit.test("Has JSON Data", function(assert){
+QUnit.test("NASA API JSON", function(assert){
 
     assert.ok(this.graphmaker.rawdata, "Loads Raw Data")
 
@@ -109,64 +85,59 @@ QUnit.test("Has JSON Data", function(assert){
 
 
 
-// accessNestedData testing
-QUnit.test("accessNestedData function should access nested data correctly", function(assert){
+//Graphmaker.prototype.accessNestedData
+QUnit.test("accessNestedData", function(assert){
 
-    var target  = [{hello: {mr: [{field: {howyoudoing: "good!"  }}]}}],
-        pointer = "howyoudoing";
+    // accessNestedData success testing
+    assert.ok(true, 'accessNestedData function should access nested data correctly');
 
-    assert.equal(this.graphmaker.accessNestedData(target, pointer), "good!", "found target")
+        var target  = [{hello: {mr: [{field: {howyoudoing: "good!"  }}]}}],
+            pointer = "howyoudoing";
 
-});
+            assert.equal(this.graphmaker.accessNestedData(target, pointer), "good!", "found target")
 
-// accessNestedData failure testing
-QUnit.test("accessNestedData function should return the failing pointer if unsuccessful", function(assert){
 
-    var target  = [{hello: {mr: [{field: {howareyou:  "good!"  }}]}}],
-        pointer = "howyoudoing";
+    // accessNestedData failure testing
+    assert.ok(true, 'accessNestedData function should return the failing pointer if unsuccessful');
 
-    assert.equal(this.graphmaker.accessNestedData(target, pointer), undefined, "captured failing pointer")
+            target  = [{hello: {mr: [{field: {howareyou:  "good!"  }}]}}],
 
-});
+            assert.equal(this.graphmaker.accessNestedData(target, pointer), undefined, "captured failing pointer")
 
-// accessNestedData stores data into array in graphmaker
-QUnit.test("accessNestedData stores data into array in graphmaker", function(assert){
+    // accessNestedData stores data into array in graphmaker
+    assert.ok(true, 'accessNestedData stores data into array in graphmaker');
 
-    var target  = JSON.parse(this.graphmaker.rawdata),
-        pointer = this.graphmaker.date;
+            target  = JSON.parse(this.graphmaker.rawdata),
+            pointer = this.graphmaker.date;
 
-    this.graphmaker.ASTEROIDS = this.graphmaker.accessNestedData(target, pointer);
+            this.graphmaker.ASTEROIDS = this.graphmaker.accessNestedData(target, pointer);
 
-    assert.ok(typeof this.graphmaker.ASTEROIDS == "object", "array of useful data stored");
+            assert.ok(typeof this.graphmaker.ASTEROIDS == "object", "array of useful data stored");
 
-});
+    // accessNestedData takes previously parsed data, then access it further
+    assert.ok(true, "accessNestedData takes previously parsed data, then access it further");
 
-// accessNestedData takes previously parsed data, then access it further
-QUnit.test("accessNestedData takes previously parsed data, then access it further", function(assert){
+            target  = this.graphmaker.ASTEROIDS[0],
+            pointer = "name";
 
-    var target  = JSON.parse(this.graphmaker.rawdata),
-        pointer = this.graphmaker.date;
+            this.graphmaker.asteroidzero = this.graphmaker.accessNestedData(target, pointer);
 
-    this.graphmaker.ASTEROIDS = this.graphmaker.accessNestedData(target, pointer);
-
-        target  = this.graphmaker.ASTEROIDS[0],
-        pointer = "name";
-
-    this.graphmaker.asteroidzero = this.graphmaker.accessNestedData(target, pointer);
-
-    assert.ok(typeof this.graphmaker.asteroidzero == "string", " retrieves name of asteroid: " + this.graphmaker.asteroidzero);
+            assert.ok(typeof this.graphmaker.asteroidzero == "string", " retrieves name of asteroid: " + this.graphmaker.asteroidzero);
 
 });
 
-// Use buildArrayFrom to generate array from data set and label/target data
 
-QUnit.test("Use buildArrayFrom to generate array from data set and label/target data", function(assert){
+//Graphmaker.prototype.buildArrayFrom
+QUnit.test("buildArrayFrom", function(assert){
 
-    var data  = [{hello: {mr: [{field: {name: "nick", age: 26}}]}}, {hello: {mr: [{field: {name: "john", age: 46}}]}}]
+    // Use buildArrayFrom to generate array from data set and label/target data
+    assert.ok(true, 'Use buildArrayFrom to generate array from data set and label/target data');
 
-    var newArray = this.graphmaker.buildArrayFrom(data, {name: "name", age: "age"});
+        var data  = [{hello: {mr: [{field: {name: "nick", age: 26}}]}}, {hello: {mr: [{field: {name: "john", age: 46}}]}}]
 
-    for (var item of newArray){
+        var newArray = this.graphmaker.buildArrayFrom(data, {name: "name", age: "age"});
+
+        for (var item of newArray){
 
         assert.ok(Object.keys(item).toString() === 'name,age', "name: " + item['name'] + " age: " + item['age']);
 
@@ -216,73 +187,59 @@ QUnit.module( "Comparing Data", {
 
 }});
 
-// compareAndBuild with real JSON
-
-QUnit.test("Use buildArrayFrom to generate array from real JSON data set and label/target data", function(assert){
-
-    var newArray = this.graphmaker.buildArrayFrom(this.graphmaker.ASTEROIDS, {name: "name", size: "estimated_diameter_min"});
-
-    for (var item of newArray){
-
-        assert.ok(Object.keys(item).toString() === 'name,size', "name: " + item['name'] + " size: " + item['size']);
-
-    }
-});
 
 // use sortBy to sortBy to take dataset and sort based on property
 
-QUnit.test("Use sortBy to sort array of objects based on proeprty value", function(assert){
+QUnit.test("sortBy", function(assert){
 
-    var array = [{age: 45},{age: 25},{age: 13},{age: 34},{age: 90}]
-    var newArray = this.graphmaker.sortBy(array, 'age');
+    // use sortBy with 'high-to-low' as third argument to reverse array after operation
+    assert.ok(true, 'Use sortBy to sort object data by property type');
 
-    var baseNum = 0;
+        var array = [{age: 45},{age: 25},{age: 13},{age: 34},{age: 90}]
+        var newArray = this.graphmaker.sortBy(array, 'age');
 
-    for (var item of newArray){
+        var baseNum = 0;
 
-        assert.ok(item['age'] >= baseNum, item['age'] + " is less or equal to...");
-        baseNum = item['age'];
+        for (var item of newArray){
 
-    }
-});
+            assert.ok(item['age'] >= baseNum, item['age'] + " is less or equal to...");
+            baseNum = item['age'];
 
-// use sortBy with 'high-to-low' as third argument to reverse array after operation
+        }
 
-QUnit.test("Use sortBy with 'high-to-low' as third argument to reverse array after operation", function(assert){
+    // use sortBy with 'high-to-low' as third argument to reverse array after operation
+    assert.ok(true, 'Use sortBy with high-to-low as third argument to reverse array after operation');
 
-    var array = [{age: 45},{age: 25},{age: 13},{age: 34},{age: 90}]
-    var newArray = this.graphmaker.sortBy(array, 'age', 'high-to-low');
+        newArray = this.graphmaker.sortBy(array, 'age', 'high-to-low');
 
-    var baseNum = Infinity;
+        baseNum = Infinity;
 
-    for (var item of newArray){
+        for (var item of newArray){
 
-        assert.ok(item['age'] <= baseNum, item['age'] + " is greater or equal to...");
-        baseNum = item['age'];
+            assert.ok(item['age'] <= baseNum, item['age'] + " is greater or equal to...");
+            baseNum = item['age'];
 
-    }
-});
+        }
 
-// use sortBy to sortBy to take real JSON dataset and sort based on property
+    // Use sortBy to sort array of real JSON data based on proeprty value
+    assert.ok(true, 'Use sortBy to sort array of real JSON data based on proeprty value');
 
-QUnit.test("Use sortBy to sort array of real JSON data based on proeprty value", function(assert){
-
-    var array = this.graphmaker.buildArrayFrom(this.graphmaker.ASTEROIDS, {name: "name", size: "estimated_diameter_min"});
+        array = this.graphmaker.buildArrayFrom(this.graphmaker.ASTEROIDS, {name: "name", size: "estimated_diameter_min"});
         newArray = this.graphmaker.sortBy(array, 'size');
 
-    var baseNum = 0;
+        baseNum = 0;
 
-    for (var item of newArray){
+        for (var item of newArray){
 
-        assert.ok(item['size'] >= baseNum, item['size'] + " is less or equal to...");
-        baseNum = item['size'];
+            assert.ok(item['size'] >= baseNum, item['size'] + " is less or equal to...");
+            baseNum = item['size'];
 
-    }
+        }
 });
 
 // use scale to take set of data, sort it and then return original data with scaled values substituted
 
-QUnit.test("Use scale to sort data, then return original data with scaled values substituted", function(assert){
+QUnit.test("scale", function(assert){
 
     var array = [{age:50},{age:40},{age:70},{age:60}];
         newArray = this.graphmaker.scale(array, 'age');
@@ -348,7 +305,7 @@ QUnit.module( "Replacement & Formatting", {
 
 // nestedReplaceWith searches objects in arrays with properties and replaces their value/formats with function
 
-QUnit.test("nestedReplaceWith can replace object values(or format those values)", function(assert){
+QUnit.test("nestedReplaceWith", function(assert){
 
     var array = [{age: 45},{age: 25},{age: 13},{age: 34},{age: 90}],
         newArray = this.graphmaker.nestedReplaceWith(array[0], 'age', 90);
@@ -370,14 +327,14 @@ QUnit.test("nestedReplaceWith can replace object values(or format those values)"
     assert.ok(newArray[1]['age'] == 40, 'Can use function to format values. 20 became 40');
 
 
+
 });
 
 // formatNumTo converts numbers with optional post string and decimal places
 
-QUnit.test("formatNumTo converts numbers with optional post string and decimal places", function(assert){
+QUnit.test("formatNumTo", function(assert){
 
     assert.equal(this.graphmaker.formatNumTo(2000), "2,000", "Can take four digit integer & format");
-    console.log(this.graphmaker.formatNumTo(2000).length)
 
     assert.equal(this.graphmaker.formatNumTo(2000.354), "2,000.354", "Can take four digit float & and format (no effect on decimals)");
 
